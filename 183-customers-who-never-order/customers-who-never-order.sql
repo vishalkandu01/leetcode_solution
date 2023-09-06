c3254a50716f94ba03ef1f1552987308ec1cf8df
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-09-06 07:39:19 +0530

    Time: 1166 ms (76.74%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -5,6 +5,11 @@
 # WHERE id NOT IN (SELECT customerId from Orders);
 
 -- Method2 --> By using JOIN
-SELECT c.name AS Customers FROM Customers AS c
-LEFT JOIN Orders AS o 
-ON c.id = o.customerID WHERE o.customerID IS NULL;
+# SELECT c.name AS Customers FROM Customers AS c
+# LEFT JOIN Orders AS o 
+# ON c.id = o.customerID WHERE o.customerID IS NULL;
+
+select name as Customers from Customers
+left join Orders 
+on Customers.id = Orders.customerID
+where Orders.customerID is NULL;
