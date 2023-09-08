9d03707147d7fa3dc9f7f99871466ef820cfffb3
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-09-08 08:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (60.77%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        vector<int> hsh(26, 0);
-        for(int i = 0; i < t.size(); i++) {
-            hsh[t[i] - 'a']++;
-        }
-
-        for(int i = 0; i < s.size(); i++) {
-            hsh[s[i] - 'a']--;
-        }
-
-        for(int i = 0; i < 26; i++) {
-            if(hsh[i] < 0) {
-                return false;
-            }
-        }
-
         int i = 0, j = 0;
         while(i < t.size() && j < s.size()) {
             if(t[i] == s[j]) {
