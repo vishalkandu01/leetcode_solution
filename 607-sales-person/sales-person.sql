744b70fb0af26544b720a845f5738276da607e8e
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-09-12 09:36:54 +0530

    Time: 2625 ms (93.30%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,9 +1,20 @@
 # Write your MySQL query statement below
-SELECT s.name
-FROM Orders o
-JOIN Company c
-ON o.com_id = c.com_id
-AND c.name = 'RED'
-RIGHT JOIN SalesPerson s
-ON s.sales_id = o.sales_id
-WHERE o.sales_id IS NULL;
+
+# Method 1
+# SELECT s.name
+# FROM Orders o
+# JOIN Company c
+# ON o.com_id = c.com_id
+# AND c.name = 'RED'
+# RIGHT JOIN SalesPerson s
+# ON s.sales_id = o.sales_id
+# WHERE o.sales_id IS NULL;
+
+
+# Method 2
+SELECT s.name FROM SalesPerson s
+WHERE s.sales_id NOT IN (
+  SELECT o.sales_id FROM Orders o
+  JOIN Company c
+  ON c.com_id = o.com_id WHERE c.name = 'RED'
+)
