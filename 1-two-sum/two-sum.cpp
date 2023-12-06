141647f4e6a46da602eddc51fc239f2f0c1fbd81
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-06 21:29:34 +0530

    Time: 3 ms (99.25%) | Memory: 11.1 MB (49.38%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -16,24 +16,35 @@ public:
 
 
         // method-2
-        vector<pair<int, int>> v;
+        // vector<pair<int, int>> v;
+        // for(int i = 0; i < n; i++) {
+        //     v.push_back({nums[i], i});
+        // }
+        // sort(v.begin(), v.end());
+        // int low, high;
+        // low = 0, high = n-1;
+        // while(low < high) {
+        //     if(v[low].first+v[high].first == target) {
+        //         return {v[low].second, v[high].second};
+        //     }
+        //     else if(v[low].first+v[high].first < target) {
+        //         low++;
+        //     }
+        //     else {
+        //         high--;
+        //     }
+        // }
+        // return {-1, -1};
+
+
+        // method-3
+        unordered_map<int, int> mpp;
         for(int i = 0; i < n; i++) {
-            v.push_back({nums[i], i});
+            if(mpp.find(target-nums[i]) != mpp.end()) {
+                return {i, mpp[target - nums[i]]};
+            }
+            mpp[nums[i]] = i;
         }
-        sort(v.begin(), v.end());
-        int low, high;
-        low = 0, high = n-1;
-        while(low < high) {
-            if(v[low].first+v[high].first == target) {
-                return {v[low].second, v[high].second};
-            }
-            else if(v[low].first+v[high].first < target) {
-                low++;
-            }
-            else {
-                high--;
-            }
-        }
-        return {-1, -1};
+        return {-1,-1};
     }
 };
