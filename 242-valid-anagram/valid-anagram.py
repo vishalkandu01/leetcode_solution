2983f528330dee3a6747bb459a4ce08224efcf53
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-06 18:58:22 +0530

    Time: 36 ms (50.49%) | Memory: 15.3 MB (6.79%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -7,5 +7,20 @@ class Solution(object):
         """
 
         # method-1
-        return Counter(s) == Counter(t)
+        # return Counter(s) == Counter(t)
+
+        # method-2
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+        for i in range (len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for cnt_ele in countS:
+            if countS[cnt_ele] != countT.get(cnt_ele, 0):
+                return False
+        
+        return True
         
