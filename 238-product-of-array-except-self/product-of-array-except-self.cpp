f472d4e3d92588566db704dcfa3420272b7e52fa
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-08 08:46:49 +0530

    Time: 22 ms (27.58%) | Memory: 24.3 MB (72.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -19,17 +19,32 @@ public:
 
 
         // method-2
-        vector<int> prefix(n, 1);
-        for(int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] * nums[i-1];
-        }
-        vector<int> suffix(n, 1);
-        for(int i = n-2; i >= 0; i--) {
-            suffix[i] = suffix[i+1] * nums[i+1];
-        }
+        // vector<int> prefix(n, 1);
+        // for(int i = 1; i < n; i++) {
+        //     prefix[i] = prefix[i-1] * nums[i-1];
+        // }
+        // vector<int> suffix(n, 1);
+        // for(int i = n-2; i >= 0; i--) {
+        //     suffix[i] = suffix[i+1] * nums[i+1];
+        // }
+        // vector<int> ans(n, 1);
+        // for(int i = 0; i < n; i++) {
+        //     ans[i] = prefix[i] * suffix[i];
+        // }
+        // return ans;
+
+
+        // method-3
         vector<int> ans(n, 1);
+        int temp = 1;
         for(int i = 0; i < n; i++) {
-            ans[i] = prefix[i] * suffix[i];
+            ans[i] = temp;
+            temp = temp * nums[i];
+        }
+        temp = 1;
+        for(int i = n-1; i >= 0; i--) {
+            ans[i] = ans[i] * temp;
+            temp = temp * nums[i];
         }
         return ans;
     }
