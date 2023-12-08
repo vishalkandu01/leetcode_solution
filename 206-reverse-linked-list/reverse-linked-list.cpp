baca94efc50f4fdfc1ad5c89b6afb7e22b328250
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-08 15:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (25.29%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -9,18 +9,37 @@
  * };
  */
 class Solution {
+private:
+    void reverse(ListNode* &head, ListNode* currNode, ListNode* prevNode) {
+        if(currNode == NULL) {
+            head = prevNode;
+            return;
+        }
+        reverse(head, currNode->next, currNode);
+        currNode->next = prevNode;
+    }
+
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prevNode = NULL;
-        ListNode* currNode = head;
-        ListNode* nextNode = NULL;
 
-        while(currNode != NULL) {
-            nextNode = currNode -> next;
-            currNode -> next = prevNode;
-            prevNode = currNode;
-            currNode = nextNode;
-        }
-        return prevNode;
+        // method-1
+        // ListNode* prevNode = NULL;
+        // ListNode* currNode = head;
+        // ListNode* nextNode = NULL;
+
+        // while(currNode != NULL) {
+        //     nextNode = currNode -> next;
+        //     currNode -> next = prevNode;
+        //     prevNode = currNode;
+        //     currNode = nextNode;
+        // }
+        // return prevNode;
+
+
+        // method-2
+        ListNode* currNode = head;
+        ListNode* prevNode = NULL;
+        reverse(head, currNode, prevNode);
+        return head;
     }
 };
