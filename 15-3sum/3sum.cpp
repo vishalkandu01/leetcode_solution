244ca3b04fccac5907a5a04e3cefe841c924619e
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-09 17:47:00 +0530

    Time: 101 ms (81.52%) | Memory: 24.2 MB (97.04%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,8 +2,51 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
+        
+        // method-1
+        // vector<vector<int>> ans;
+        // for(int i = 0; i < n-2; i++) {
+        //     for(int j = i+1; j < n-1; j++) {
+        //         for(int k = j+1; k < n; k++) {
+        //             if(nums[i] + nums[j] + nums[k] == 0) {
+        //                 ans.push_back({nums[i], nums[j], nums[k]});
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
 
-        set<vector<int>> st;
+
+        // method-2
+        // set<vector<int>> st;
+        // sort(nums.begin(), nums.end());
+        // for(int i = 0; i < n; i++) {
+        //     int start = i+1;
+        //     int end = n-1;
+        //     while(start < end) {
+        //         int targetValue = nums[i] + nums[start] + nums[end];
+        //         if(targetValue == 0) {
+        //             st.insert({nums[i], nums[start], nums[end]});
+        //             start++;
+        //             end--;
+        //         }
+        //         else if(targetValue < 0) {
+        //             start++;
+        //         }
+        //         else {
+        //             end--;
+        //         }
+        //     }
+        // }
+        // vector<vector<int>> ans;
+        // for(auto it : st) {
+        //     ans.push_back(it);
+        // }
+        // return ans;
+
+
+        // method-3
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n; i++) {
             int start = i+1;
@@ -11,7 +54,9 @@ public:
             while(start < end) {
                 int targetValue = nums[i] + nums[start] + nums[end];
                 if(targetValue == 0) {
-                    st.insert({nums[i], nums[start], nums[end]});
+                    ans.push_back({nums[i], nums[start], nums[end]});
+                    while(start < end && nums[start] == nums[start+1])start++;
+                    while(start < end && nums[end] == nums[end-1]) end--;
                     start++;
                     end--;
                 }
@@ -22,11 +67,7 @@ public:
                     end--;
                 }
             }
-        }
-
-        vector<vector<int>> ans;
-        for(auto it : st) {
-            ans.push_back(it);
+            while(i+1 <= n-1 && nums[i] == nums[i+1]) i++;
         }
         return ans;
     }
