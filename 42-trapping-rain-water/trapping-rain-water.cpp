0e75e75692d110190951e0eca9620ca108be39ed
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-10 18:25:11 +0530

    Time: 14 ms (36.82%) | Memory: 20.2 MB (49.98%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -19,20 +19,41 @@ public:
         // return res;
 
 
-        // method-2
-        vector<int> leftMax(n, 0);
-        leftMax[0] = height[0];
-        for(int i = 1; i < n; i++) {
-            leftMax[i] = max(leftMax[i-1], height[i]);
-        }
-        vector<int> rightMax(n, 0);
-        rightMax[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--) {
-            rightMax[i] = max(rightMax[i+1], height[i]);
-        }
+        // method-2 -> Precalculation method -> it's a easy one
+        // vector<int> leftMax(n, 0);
+        // leftMax[0] = height[0];
+        // for(int i = 1; i < n; i++) {
+        //     leftMax[i] = max(leftMax[i-1], height[i]);
+        // }
+        // vector<int> rightMax(n, 0);
+        // rightMax[n-1] = height[n-1];
+        // for(int i = n-2; i >= 0; i--) {
+        //     rightMax[i] = max(rightMax[i+1], height[i]);
+        // }
+        // int res = 0;
+        // for(int i = 0; i < n; i++) {
+        //     res = res + min(leftMax[i], rightMax[i]) - height[i];
+        // }
+        // return res;
+
+
+        // method-3
+        int left, right;
+        left = 0, right = n-1;
+        int leftMax, rightMax;
+        leftMax = 0, rightMax = 0;
         int res = 0;
-        for(int i = 0; i < n; i++) {
-            res = res + min(leftMax[i], rightMax[i]) - height[i];
+        while(left <= right) {
+            if(rightMax <= leftMax) {
+                res = res + max(0, rightMax - height[right]);
+                rightMax = max(rightMax, height[right]);
+                right = right - 1;
+            }
+            else {
+                res = res + max(0, leftMax - height[left]);
+                leftMax = max(leftMax, height[left]);
+                left = left + 1;
+            }
         }
         return res;
     }
