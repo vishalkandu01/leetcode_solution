d2920fc6fa226fd25d1419df7a9dd310fdbd49f5
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-12 18:04:16 +0530

    Time: 56 ms (9.80%) | Memory: 23 MB (6.63%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+
+        
+        // method-1
+        if(intervals.empty()) {
+            return vector<vector<int>> ();
+        }
+        sort(intervals.begin(), intervals.end());
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < intervals[i].size(); j++) {
+                cout << intervals[i][j];
+            }
+        }
+        stack<vector<int>> st;
+        st.push(intervals[0]);
+        for(int i = 1; i < n; i++) {
+            vector<int> current = intervals[i];
+            vector<int> top = st.top();
+            if(top[1] >= current[0]) {
+                top[1] = max(top[1], current[1]);
+                st.pop();
+                st.push(top);
+
+            } 
+            else {
+                st.push(current);
+            }
+        }
+        intervals.clear();
+        while(!st.empty()) {
+            intervals.push_back(st.top());
+            st.pop();
+        }
+        return intervals;
+    }
+};
