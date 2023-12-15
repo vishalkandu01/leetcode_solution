0cab4b246d56bf4864f71a721389b58f9615af02
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-15 07:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (66.88%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,20 +11,32 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int len = 0;
-        ListNode* temp = head;
-        if(head -> next == NULL) return head;
-        while(temp != NULL) {
-            temp = temp -> next;
-            len += 1;
+
+        // method-1
+        // int len = 0;
+        // ListNode* temp = head;
+        // if(head -> next == NULL) return head;
+        // while(temp != NULL) {
+        //     temp = temp -> next;
+        //     len += 1;
+        // }
+        // len = len/2;
+        // // cout << len;
+        // temp = head;
+        // while(len != 0) {
+        //     temp = temp -> next;
+        //     len -= 1;
+        // }
+        // return temp;
+
+
+        // method-2
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast -> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
         }
-        len = len/2;
-        // cout << len;
-        temp = head;
-        while(len != 0) {
-            temp = temp -> next;
-            len -= 1;
-        }
-        return temp;
+        return slow;
     }
 };
