0d8558faf49be52e94a9c446717d26326a9422fa
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-15 07:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (10.33%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -14,22 +14,17 @@ public:
         int len = 0;
         ListNode* temp = head;
         if(head -> next == NULL) return head;
-        len += 1;
         while(temp != NULL) {
             temp = temp -> next;
             len += 1;
         }
-        bool flag = false;
-        if(len % 2 != 0) flag = true;
         len = len/2;
         // cout << len;
         temp = head;
-        len -= 1;
         while(len != 0) {
             temp = temp -> next;
             len -= 1;
         }
-        if(flag == false) return temp;
-        return temp -> next;
+        return temp;
     }
 };
