b276c71adfd558eea45fe0db41e8ceb3fc62414f
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-15 11:40:33 +0530

    Time: 13 ms (29.07%) | Memory: 10.9 MB (71.53%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -3,17 +3,29 @@ public:
     string destCity(vector<vector<string>>& paths) {
         int n = paths.size();
 
-        unordered_map<string, int> mpp;
-        for(int i = 0; i < n; i++) {
-            mpp[paths[i][0]] = 1;
-        }
+        // method-1
+        // unordered_map<string, int> mpp;
+        // for(int i = 0; i < n; i++) {
+        //     mpp[paths[i][0]] = 1;
+        // }
+        // for(int i = 0; i < n; i++) {
+        //     if(mpp.find(paths[i][1]) == mpp.end()) {
+        //         return paths[i][1];
+        //     }
+        // }
+        // return "";
 
+
+        // method-2
+        unordered_set<string> st;
         for(int i = 0; i < n; i++) {
-            if(mpp.find(paths[i][1]) == mpp.end()) {
+            st.insert(paths[i][0]);
+        }
+        for(int i = 0; i < n; i++) {
+            if(st.find(paths[i][1]) == st.end()) {
                 return paths[i][1];
             }
         }
-
         return "";
     }
 };
