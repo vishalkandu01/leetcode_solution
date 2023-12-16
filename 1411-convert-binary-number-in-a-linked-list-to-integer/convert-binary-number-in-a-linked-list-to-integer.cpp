700d7f1eda98924c542b270018388fc18e6d3e86
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-16 10:23:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.06%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -11,16 +11,39 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        vector<int> v;
-        ListNode* temp = head;
-        while(temp != NULL) {
-            v.push_back(temp->val);
-            temp = temp -> next;
+        
+        // method-1
+        // vector<int> v;
+        // ListNode* temp = head;
+        // while(temp != NULL) {
+        //     v.push_back(temp->val);
+        //     temp = temp -> next;
+        // }
+        // reverse(v.begin(), v.end());
+        // int ans = 0;
+        // for(int i = 0; i < v.size(); i++) {
+        //     ans += v[i] * pow(2, i);
+        // }
+        // return ans;
+
+
+        // method-2
+        ListNode* curr = head;
+        ListNode *prev = NULL, *next = NULL;
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
         }
-        reverse(v.begin(), v.end());
+        head = prev;
         int ans = 0;
-        for(int i = 0; i < v.size(); i++) {
-            ans += v[i] * pow(2, i);
+        int i = 0;
+        curr = head;
+        while(curr != NULL) {
+            ans += curr->val * pow(2, i);
+            i++;
+            curr = curr -> next;
         }
         return ans;
     }
