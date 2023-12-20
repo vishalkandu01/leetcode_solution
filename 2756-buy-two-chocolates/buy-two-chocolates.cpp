2663b6a7ade6ab15427aa495f0cc9dd4f8854bb7
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-20 17:41:20 +0530

    Time: 11 ms (73.75%) | Memory: 46.9 MB (94.77%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        int n = prices.size();
-        
         sort(prices.begin(), prices.end());
         int chocolatePrice = prices[0] + prices[1];
         if(chocolatePrice > money) return money;
