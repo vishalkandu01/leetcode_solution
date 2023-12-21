bd5f40a3682b962f4b8a484f3ffc1d1b85b04f02
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-21 14:13:15 +0530

    Time: 32 ms (20.20%) | Memory: 19.4 MB (9.57%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,24 +11,49 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        // method-1
         vector<ListNode*> v;
         ListNode* temp = head;
         while(temp != NULL) {
             v.push_back(temp);
             temp = temp -> next;
         }
-        int i = 1;
+        int i = 0;
         int j = v.size()-1;
-        temp = head;
-        while(i <= j) {
-            temp -> next = v[j];
+        head -> next = v[j];
+        i++;
+        temp = head -> next;
+        while(i < j) {
+            temp -> next = v[i];
             temp = temp -> next;
             j--;
-            temp -> next = v[i];
+            temp -> next = v[j];
             temp = temp -> next;
             i++;
         }
         temp -> next = NULL;
         return;
+
+
+        // method-2
+        // vector<ListNode*> v;
+        // ListNode* temp = head;
+        // while(temp != NULL) {
+        //     v.push_back(temp);
+        //     temp = temp -> next;
+        // }
+        // int i = 1;
+        // int j = v.size()-1;
+        // temp = head;
+        // while(i <= j) {
+        //     temp -> next = v[j];
+        //     temp = temp -> next;
+        //     j--;
+        //     temp -> next = v[i];
+        //     temp = temp -> next;
+        //     i++;
+        // }
+        // temp -> next = NULL;
+        // return;
     }
 };
