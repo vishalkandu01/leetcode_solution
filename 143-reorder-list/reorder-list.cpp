ff9ba976e09c4a5b4f3f170c5cda4fa409925cc2
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-12-21 15:59:56 +0530

    Time: 28 ms (40.63%) | Memory: 18.2 MB (52.40%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -12,27 +12,27 @@ class Solution {
 public:
     void reorderList(ListNode* head) {
         // method-1
-        vector<ListNode*> v;
-        ListNode* temp = head;
-        while(temp != NULL) {
-            v.push_back(temp);
-            temp = temp -> next;
-        }
-        int i = 0;
-        int j = v.size()-1;
-        head -> next = v[j];
-        i++;
-        temp = head -> next;
-        while(i < j) {
-            temp -> next = v[i];
-            temp = temp -> next;
-            j--;
-            temp -> next = v[j];
-            temp = temp -> next;
-            i++;
-        }
-        temp -> next = NULL;
-        return;
+        // vector<ListNode*> v;
+        // ListNode* temp = head;
+        // while(temp != NULL) {
+        //     v.push_back(temp);
+        //     temp = temp -> next;
+        // }
+        // int i = 0;
+        // int j = v.size()-1;
+        // head -> next = v[j];
+        // i++;
+        // temp = head -> next;
+        // while(i < j) {
+        //     temp -> next = v[i];
+        //     temp = temp -> next;
+        //     j--;
+        //     temp -> next = v[j];
+        //     temp = temp -> next;
+        //     i++;
+        // }
+        // temp -> next = NULL;
+        // return;
 
 
         // method-2
@@ -55,5 +55,36 @@ public:
         // }
         // temp -> next = NULL;
         // return;
+
+
+        // method-3
+        if(head == NULL) return;
+        ListNode* temp = head;
+        ListNode* half = head;
+        ListNode* prev = NULL;
+        while(temp -> next != NULL && temp -> next -> next != NULL) {
+            temp = temp -> next -> next;
+            half = half -> next;
+        }
+        if(temp -> next != NULL) half = half -> next;
+        while(half != NULL) {
+            temp = half -> next;
+            half -> next = prev;
+            prev = half;
+            half = temp;
+        } 
+        half = prev;
+        while(head != NULL && half != NULL) {
+            temp = head -> next;
+            prev = half -> next;
+            head -> next = half;
+            half -> next = temp;
+            head = temp;
+            half = prev;
+        }
+        if(head != NULL && head -> next != NULL) {
+            head -> next -> next = NULL;
+        }
+        return;
     }
 };
