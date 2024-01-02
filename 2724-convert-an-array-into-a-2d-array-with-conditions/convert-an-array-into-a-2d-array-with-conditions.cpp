793224bdb771c5729ab9910c681da1804a8c3a7c
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2024-01-02 21:18:57 +0530

    Time: 7 ms (90.89%) | Memory: 29.8 MB (83.60%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -2,20 +2,34 @@ class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> mpp;
-        int ctr = 0;
-        for(int i = 0; i < n; i++) {
-            mpp[nums[i]]++;
-            ctr = max(ctr, mpp[nums[i]]);
-        }
 
-        vector<vector<int>> ans(ctr);
-        for(auto it : mpp) {
-            int ele = it.first;
-            int freq = it.second;
-            for(int i = 0; i < freq; i++) {
-                ans[i].push_back(ele);
+        // method-1
+        // unordered_map<int, int> mpp;
+        // int ctr = 0;
+        // for(int i = 0; i < n; i++) {
+        //     mpp[nums[i]]++;
+        //     ctr = max(ctr, mpp[nums[i]]);
+        // }
+        // vector<vector<int>> ans(ctr);
+        // for(auto it : mpp) {
+        //     int ele = it.first;
+        //     int freq = it.second;
+        //     for(int i = 0; i < freq; i++) {
+        //         ans[i].push_back(ele);
+        //     }
+        // }
+        // return ans;
+
+
+        // method-2
+        vector<int> frequency(n+1);
+        vector<vector<int>> ans;
+        for(int it : nums) {
+            if(frequency[it] >= ans.size()) {
+                ans.push_back({});
             }
+            ans[frequency[it]].push_back(it);
+            frequency[it]++;
         }
         return ans;
     }
