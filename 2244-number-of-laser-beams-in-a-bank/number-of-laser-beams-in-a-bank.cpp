f0e4c431fe71262eb4d34682d512756517d663d1
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2024-01-03 21:21:18 +0530

    Time: 93 ms (47.47%) | Memory: 35.1 MB (5.06%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -10,14 +10,38 @@ private:
 
 public:
     int numberOfBeams(vector<string>& bank) {
-        int prevRowCount = 0;
-        int total = 0;
-        for(auto row : bank) {
-            int curRowCount = calculate(row);
-            if(curRowCount == 0) continue;
-            total += curRowCount * prevRowCount;
-            prevRowCount = curRowCount;
+        
+        // method-1
+        // int prevRowCount = 0;
+        // int total = 0;
+        // for(auto row : bank) {
+        //     int curRowCount = calculate(row);
+        //     if(curRowCount == 0) continue;
+        //     total += curRowCount * prevRowCount;
+        //     prevRowCount = curRowCount;
+        // }
+        // return total;
+  
+  
+        // method-2
+        int ans = 0;
+        vector<int> v;
+        for(auto it : bank) {
+            string str = it;
+            int cnt = 0;
+            for(int i = 0; i < str.size(); i++) {
+                if(str[i] == '1') {
+                    cnt++;
+                }
+            }
+            if(cnt != 0) {
+                v.push_back(cnt);
+            }
         }
-        return total;
+        for(int i = 1; i < v.size(); i++) {
+            int t = v[i] * v[i-1];
+            ans += t;
+        }
+        return ans;
     }
 };
