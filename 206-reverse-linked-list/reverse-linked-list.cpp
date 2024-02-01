c769e7cb74cde3443d8b0a30efff187e32ee90f5
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2024-02-02 01:26:30 +0530

    Time: 4 ms (48.26%) | Memory: 11.6 MB (15.76%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,36 +10,39 @@
  */
 class Solution {
 private:
-    void reverse(ListNode* &head, ListNode* currNode, ListNode* prevNode) {
-        if(currNode == NULL) {
-            head = prevNode;
-            return;
-        }
-        reverse(head, currNode->next, currNode);
-        currNode->next = prevNode;
+    ListNode* solve(ListNode* curr, ListNode* prev, ListNode* forw) {
+        if(curr == NULL) return prev;
+        forw = curr -> next;
+        curr -> next = prev;
+
+        // one way to write
+        // prev = curr;
+        // curr = forw;
+        // return solve(curr, prev, forw);
+        
+        // another way to write
+        return solve(forw, curr, forw);
     }
 
 public:
     ListNode* reverseList(ListNode* head) {
 
-        // method-1
-        // ListNode* prevNode = NULL;
-        // ListNode* currNode = head;
-        // ListNode* nextNode = NULL;
-
-        // while(currNode != NULL) {
-        //     nextNode = currNode -> next;
-        //     currNode -> next = prevNode;
-        //     prevNode = currNode;
-        //     currNode = nextNode;
+        // method-1 -> iterative approach
+        // ListNode* curr = head;
+        // ListNode* prev = NULL;
+        // ListNode* forw = NULL;
+        // while(curr != NULL) {
+        //     forw = curr -> next;
+        //     curr -> next = prev;
+        //     prev = curr;
+        //     curr = forw;
         // }
-        // return prevNode;
+        // return prev;
 
-
-        // method-2
-        ListNode* currNode = head;
-        ListNode* prevNode = NULL;
-        reverse(head, currNode, prevNode);
-        return head;
+        // method-2 -> recrusive solution
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* forw = NULL;
+        return solve(curr, prev, forw); 
     }
 };
