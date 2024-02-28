d9ac33ea26c1fdf00213b9863d795b6912762682
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2024-02-28 16:31:21 +0530

    Time: 7 ms (96.52%) | Memory: 20.3 MB (52.96%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -15,10 +15,14 @@ public:
 
 
         // method-2
-        int x = 0;
+        int xor1 = 0;
+        int xor2 = 0;
+        // int x = 0;
         for(int i = 0; i < nums.size(); i++) {
-                x = x ^ ( (i+1) ^ nums[i] );
+                xor1 = xor1 ^ nums[i];
+                xor2 = xor2 ^ (i+1);
+                // x = x ^ ( nums[i] ^ (i+1) );
         }
-        return x;
+        return xor1 ^ xor2;
     }
 };
